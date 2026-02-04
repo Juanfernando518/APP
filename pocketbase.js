5bd22885382a72c30eb5afcadccc0604a6c269b9
jfernandoalz18@gmail.com <jfernandoalz18@gmail.com>
2026-02-03 22:49:45 -0500

    Actualización de URLs de archivos para ngrok

--- pocketbase.js
+++ pocketbase.js
@@ -1,9 +1,10 @@
-// Importamos el SDK (si usas CDN) o simplemente definimos la URL
-const pbUrl = "http://127.0.0.1:8090"; 
+// CAMBIO CRÍTICO: Usamos la URL de ngrok para que las imágenes sean públicas
+const pbUrl = "https://trifid-kerry-nonunitable.ngrok-free.dev"; 
 
-// Esta función nos servirá para obtener las imágenes de los posters/videos
+// Esta función ahora añade el parámetro para saltar el aviso de ngrok en las imágenes
 export const getFileUrl = (record, filename) => {
-    return `${pbUrl}/api/files/${record.collectionId}/${record.id}/${filename}`;
+    if (!filename) return 'https://via.placeholder.com/200x280';
+    return `${pbUrl}/api/files/${record.collectionId}/${record.id}/${filename}?ngrok-skip-browser-warning=1`;
 };
 
 export default pbUrl;
